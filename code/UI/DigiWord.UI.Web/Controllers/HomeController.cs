7bc782c4457c3d93801b940c6c0fcba76d59109f
Behnam Karimi <disorian@gmail.com>
2018-02-21 19:58:14 +0800

    completes UI

---
+++ code/UI/DigiWord.UI.Web/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using DigiWord.Entities;
+using DigiWord.UI.Process;
+using DigiWord.UI.Process.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DigiWord.UI.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(NumberDetailViewModel numberDetailViewModel)
+        {
+            try
+            {
+                ConverterProcess process = new ConverterProcess();
+
+                NumberDetail result = process.ConvertNumebrDetail(numberDetailViewModel);
+
+                return View("Result", result);
+            }
+            catch(Exception)
+            {
+                return View();
+            }
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+        
+    }
+}
