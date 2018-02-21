5bb6b8c444ffaaeeac36dfe7f7242e0360d116fd
Behnam Karimi <disorian@gmail.com>
2018-02-21 09:23:26 +0800

    adds number detail entity, ulong extension and completes the business component

---
+++ code/DigiWord.Business/Extensions/NumberConverterExtension.cs
@@ -0,0 +1,29 @@
+﻿using DigiWord.Business.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigiWord.Business.Extensions
+{
+    /// <summary>
+    /// Extention methods
+    /// </summary>
+    public static class NumberConverterExtension
+    {
+        /// <summary>
+        /// Extends System.UInt64 and returns the textual representation of its value
+        /// </summary>
+        /// <param name="number">The number to be converted to text</param>
+        /// <returns>A steing holds the textual representation of the given number</returns>
+        public static string ToText(this ulong number)
+        {
+            NumberConverter converter = new NumberConverter();
+
+            string convertedNumber = converter.Convert(number);
+
+            return convertedNumber;
+        }
+    }
+}
---
+++ code/DigiWord.Entities/NumberDetail.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace DigiWord.Entities
+{
+    /// <summary>
+    /// Represents a number detail
+    /// </summary>
+    [DataContract]
+    public class NumberDetail
+    {
+        /// <summary>
+        /// A guid holds the Id
+        /// </summary>
+        public Guid Id { get; } = Guid.NewGuid();
+
+        /// <summary>
+        /// A string holds the name
+        /// </summary>
+        [DataMember]
+        [Required]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Holds  number to be converted to textual representation
+        /// </summary>
+        [DataMember]
+        [Required]
+        public decimal Number { get; set; }
+
+        /// <summary>
+        /// Holds the textual representation if the number.
+        /// </summary>
+        [DataMember]
+        public string ConvertedNumber { get; set; }
+
+        /// <summary>
+        /// A Date-time when the record has been created
+        /// </summary>
+        public DateTime DateCreated { get; } = DateTime.UtcNow;
+
+        public override string ToString() =>
+            $"{this.Number}: {this.ConvertedNumber}";
+    }
+}
